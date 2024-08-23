7f01fcf383a4aff5b360c4f22218adedf6a0947c
Coder Buddy <118363998+CoderBuddy01@users.noreply.github.com>
2024-08-24 00:44:20 +0530

    error fixed2

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,14 +1,21 @@
 import { auth } from "@/auth"
 
 export default auth((req) => {
-    const path = req.nextUrl.pathname
-    console.log(req.auth)
-    if (!req.auth && (path == '/dashboard' || path.startsWith('/view') || path.startsWith('/play'))) {
-        console.log("Yessss")
-        return Response.redirect(new URL('/', req.nextUrl.origin));
-    }
-    else if (req.auth && path == '/') {
-        return Response.redirect(new URL('/dashboard', req.nextUrl.origin));
+    try {
+        const path = req.nextUrl.pathname
+        console.log(req.auth)
+        if (!req.auth && (path == '/dashboard' || path.startsWith('/view') || path.startsWith('/play'))) {
+            console.log("Yessss")
+            return Response.redirect(new URL('/', req.nextUrl.origin));
+        } else if (req.auth && path == '/') {
+            return Response.redirect(new URL('/dashboard', req.nextUrl.origin));
+        }
+        // Handle favicon.ico request
+        if (path === '/favicon.ico') {
+            return new Response(null, { status: 204 }); // No Content
+        }
+    } catch (error) {
+        console.error("Middleware error:", error);
+        return new Response("Internal Server Error", { status: 500 });
     }
 })
-
