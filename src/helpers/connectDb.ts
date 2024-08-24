14a77564bafb6d69fbf88c5e110fe2fba875b459
Coder Buddy <118363998+CoderBuddy01@users.noreply.github.com>
2024-08-24 19:27:48 +0530

    trying 10

--- src/helpers/connectDb.ts
+++ src/helpers/connectDb.ts
@@ -1,30 +1,20 @@
-import { error } from "console";
-import mysql from "mysql2/promise";
-import { RowDataPacket } from "mysql2";
-// import pool from "./pool";
+import serverlessMysql from 'serverless-mysql';
 
-export default async function query({ query, values }: { query: String, values: any[] }) {
-    const pool = mysql.createPool({
+const db = serverlessMysql({
+    config: {
         host: process.env.DB_HOST,
         user: process.env.DB_USER,
         password: process.env.DB_PASSWORD,
         database: process.env.DB_NAME,
-        waitForConnections: true,
-        connectionLimit: 10,
-        queueLimit: 0,
-    });
-    if (typeof query !== 'string' || !Array.isArray(values)) {
-        console.log("dtfgvbhjnkmlkguyfygvbhjnk")
-        throw new Error('Invalid arguments: query must be a string and values must be an array');
     }
+});
 
+export async function query({ query, values }: { query: String, values: any[] }) {
     try {
-        console.log(query);
-        // console.log(values);
-        const [rows] = await pool.query<RowDataPacket[]>(query, values);
-        return rows;
-    } catch (err) {
-        console.error('Error querying the database:', err);
-        throw err;
+        const results = await db.query(query, values);
+        await db.end();
+        return results;
+    } catch (error: any) {
+        throw Error(error.message);
     }
 }
