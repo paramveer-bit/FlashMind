a86e8ab64ed9ecedf69723cacd5c99c796eb8af3
Coder Buddy <118363998+CoderBuddy01@users.noreply.github.com>
2024-08-24 16:57:27 +0530

    trying 7

--- src/helpers/connectDb.ts
+++ src/helpers/connectDb.ts
@@ -1,31 +1,24 @@
-import { error } from "console"
-import mysql from "mysql2/promise"
+import mysql from "mysql2/promise";
 import { RowDataPacket } from "mysql2";
 
+const pool = mysql.createPool({
+    uri: process.env.CONNECTION_URI,
+    waitForConnections: true,
+    connectionLimit: 10,
+    queueLimit: 0
+});
 
 const query = async ({ query, values = [] }: {
     query: string,
     values: any[]
 }) => {
     try {
-        const connection = await mysql.createConnection({
-            uri: process.env.CONNECTION_URI,
-
-        })
-        try {
-            const [res] = await connection.execute(query, values)
-            connection.end()
-            return res as RowDataPacket[]
-        } catch (error: any) {
-            console.log("Somthing wrong happen while executing query")
-            throw error
-        }
-
+        const [res] = await pool.execute(query, values);
+        return res as RowDataPacket[];
     } catch (error: any) {
-        console.log("Error in connecting to database")
-        throw error?.code + " , " + error?.message
+        console.log("Something went wrong while executing query", error);
+        throw error;
     }
-
 }
 
-export default query
+export default query;
