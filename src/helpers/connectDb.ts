31d4709325f20e9aef0d8241b30470d692418be1
Coder Buddy <118363998+CoderBuddy01@users.noreply.github.com>
2024-08-24 19:23:58 +0530

    chore: Update trustedHosts in auth.ts

--- src/helpers/connectDb.ts
+++ src/helpers/connectDb.ts
@@ -1,9 +1,18 @@
 import { error } from "console";
 import mysql from "mysql2/promise";
 import { RowDataPacket } from "mysql2";
-import pool from "./pool";
+// import pool from "./pool";
 
 export default async function query({ query, values }: { query: String, values: any[] }) {
+    const pool = mysql.createPool({
+        host: process.env.DB_HOST,
+        user: process.env.DB_USER,
+        password: process.env.DB_PASSWORD,
+        database: process.env.DB_NAME,
+        waitForConnections: true,
+        connectionLimit: 10,
+        queueLimit: 0,
+    });
     if (typeof query !== 'string' || !Array.isArray(values)) {
         console.log("dtfgvbhjnkmlkguyfygvbhjnk")
         throw new Error('Invalid arguments: query must be a string and values must be an array');
