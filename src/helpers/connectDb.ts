07e0993d6af9d6a5fb603434a06f907fc0559c66
Coder Buddy <118363998+CoderBuddy01@users.noreply.github.com>
2024-08-24 19:15:25 +0530

    trying 10

--- src/helpers/connectDb.ts
+++ src/helpers/connectDb.ts
@@ -1,28 +1,21 @@
-import { error } from "console"
-import mysql from "mysql2/promise"
+import { error } from "console";
+import mysql from "mysql2/promise";
 import { RowDataPacket } from "mysql2";
+import pool from "./pool";
 
-
-const query = async ({ query, values = [] }: {
-    query: string,
-    values: any[]
-}) => {
-    const connection = await mysql.createConnection({
-        uri: process.env.CONNECTION_URI,
-
-    })
-    try {
-        const [res] = await connection.execute(query, values)
-        connection.end()
-        return res as RowDataPacket[]
-    } catch (error: any) {
-        console.log("Somthing wrong happen while executing query")
-        throw Error(error.mesaage)
-        // return { error }
+export default async function query({ query, values }: { query: String, values: any[] }) {
+    if (typeof query !== 'string' || !Array.isArray(values)) {
+        console.log("dtfgvbhjnkmlkguyfygvbhjnk")
+        throw new Error('Invalid arguments: query must be a string and values must be an array');
     }
 
-
-
+    try {
+        console.log(query);
+        // console.log(values);
+        const [rows] = await pool.query<RowDataPacket[]>(query, values);
+        return rows;
+    } catch (err) {
+        console.error('Error querying the database:', err);
+        throw err;
+    }
 }
-
-export default query
